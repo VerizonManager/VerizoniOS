e64c4943cdc2e6b3ec9877405e9273cacbbf718c
betsy john <betsy.john@infosys.com>
2020-03-05 19:35:59 +0530

    Initial Commit

---
+++ Example/Pods/Target Support Files/Pods-VerizoniOS_Tests/Pods-VerizoniOS_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VerizoniOS_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_VerizoniOS_Tests
+@end
---
+++ Example/Pods/Target Support Files/VerizoniOS/VerizoniOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_VerizoniOS : NSObject
+@end
+@implementation PodsDummy_VerizoniOS
+@end
